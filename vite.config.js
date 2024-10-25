55817530f428ba6f81c96aa77427360cac1a6041
Gifarfajar321 <gifarifajarm174@gmail.com>
2024-10-25 13:12:19 +0700

    test

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/questforgame-web/",
-  // build: {
-  //   outDir: "dist",
-  // },
-  // server: {
-  //   historyApiFallback: true,
-  // },
 });
