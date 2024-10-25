6162bda40ed333ebce825afc7de07f6c1d03bbbe
Gifarfajar321 <gifarifajarm174@gmail.com>
2024-10-25 13:06:53 +0700

    coba deploy to vercel

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/questforgame-web/",
-  build: {
-    outDir: "dist",
-  },
-  server: {
-    historyApiFallback: true,
-  },
+  // base: "/questforgame-web/",
+  // build: {
+  //   outDir: "dist",
+  // },
+  // server: {
+  //   historyApiFallback: true,
+  // },
 });
